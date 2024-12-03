e2270298db7abb5490192e5588e4e4d028415fba
Razscal <xadava198@gmail.com>
2024-12-03 22:09:43 +0700

    add websockets lib

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from routers import file
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -6,9 +6,15 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 app.include_router(file.router)
 
-origins = ["https://localhost:5173"]
+origins = ["http://localhost:63343"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"],
                    allow_headers=["*"])
 
 app.mount("/files", StaticFiles(directory="files"), name="files")
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
